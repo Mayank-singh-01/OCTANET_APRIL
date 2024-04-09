14cf7672ff5008042f0deda1d177b33acbf08df2
Mayank singh <mayank23378@gmail.com>
2024-04-10 03:32:07 +0530

    done with the javascript and project is done

--- to-do/todo.js
+++ to-do/todo.js
@@ -1,1 +1,27 @@
+const input = document.querySelector("#input-area");
+const btn = document.querySelector("#my-btn");
+const myul = document.querySelector(".myul");
 
+btn.addEventListener("click", function () {
+    if (input.value.trim() === "") {
+        alert("Please enter a task.");
+    } else {
+        const li = document.createElement("li");
+        const span = document.createElement("span");
+        span.textContent = "\u00D7";
+        li.textContent = input.value;
+        li.appendChild(span);
+        myul.appendChild(li);
+        li.addEventListener("click", () => {
+          if(li.style.textDecoration === "line-through"){
+            li.style.textDecoration= "none";
+          }else {
+            li.style.textDecoration = "line-through";
+          }
+        });
+        span.addEventListener("click", () => {
+         li.remove()
+        });
+        input.value = "";
+    }
+});
